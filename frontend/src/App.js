3e741517c7c4f56f6348267d50a1ffc097577a3e
KomjatiEvelin <komjatievelin@gmail.com>
2021-02-25 12:38:25 +0100

    Kezdetleges NavBar készítése

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+
+
 import Button from "./Button";
+import NavBar from "./component/element/NavBar";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          tasdasdasdasdasd
-        </a>
-        <Button>
-
-        </Button>
-      </header>
+        <header className="App-header">
+            <NavBar/>
+        </header>
+        <body  className="App-body">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Opening page
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </body>
     </div>
   );
 }
