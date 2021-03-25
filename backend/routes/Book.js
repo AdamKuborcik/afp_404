3e3713e59284db72f8ebf7b30045fe1229e0333f
Erdélyi Roland <erdelyi.roland99@icloud.com>
2021-03-25 10:28:15 +0100

    #23 Adott id_val rendelkező könyv törlése funkció megvalósítása

--- backend/routes/Book.js
+++ backend/routes/Book.js
@@ -5,7 +5,24 @@ const url = "mongodb+srv://user1:4VUnYWQAUO8daRVB@cluster0.apeyl.mongodb.net/myF
 const mongodb = require('mongodb');
 
 //List All
+
 //Delete in id
+router.post('/deletedata',function (req,res,next) {
+        let stringToJson=chunk.toString();
+        let json =JSON.parse(stringToJson);
+        let id=new mongodb.ObjectID(json["_id"]);
+        MongoClient.connect(url,function (err,db) {
+            if(err) throw err;
+            let dbo=db.db("mydb");
+            dbo.collection("Book").deleteMany({_id:id}).toArray(function (err,result) {
+                if (err) throw err;
+                console.log(result);
+                db.close();
+                res.send(result);
+            })
+    })
+});
+
 //Find one element
 router.get('/findOne',function (req,res) {
     req.on('data', function(chunk) {
