27659f2e77908c24d04f3aff8b4dacf60976c071
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-14 16:25:15 +0200

    NavBar megjelenés szerkesztése

--- frontend/src/components/elements/NavBar.js
+++ frontend/src/components/elements/NavBar.js
@@ -5,14 +5,15 @@ class NavBar extends React.Component
 {
     render() {
         return(
+            <header  className="App-header">
             <Navbar className="navbar">
                 <Nav>
                     <Nav.Link className="navlink" href="/">Home</Nav.Link>
                     <Nav.Link className="navlink" href="/register">Register</Nav.Link>
                     <Nav.Link className="navlink" href="/login">Login</Nav.Link>
-                    <Nav.Link className="navlink" href="#link2">Link2</Nav.Link>
                 </Nav>
             </Navbar>
+            </header>
         )
 
     }
