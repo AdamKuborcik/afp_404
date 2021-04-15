c3bbeb74eff8dc3b88ae3fea18346aed69ee3a50
Donkó Márk <donkomark@gmail.com>
2021-04-15 10:19:21 +0200

    GetBook megvalósitva fetchel a beckendtől

---
+++ frontend/src/actions/GetBooks.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import BookDispatcher from '../dispatcher/BookDispatcher';
+import * as actionConstants from '../dispatcher/ActionConstans'
+
+export const GetBooks = () => {
+    axios.defaults.timeout = 1500;
+    axios.get('/employee').then((resp) => {
+        BookDispatcher.dispatch({
+            action: actionConstants.refresh,
+            payload: resp.data
+        })
+    })
+}
