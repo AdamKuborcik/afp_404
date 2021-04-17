5ad6c53a28970584e06f44fd954238f24bc089ff
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-17 17:44:48 +0200

    ErrorStore belső szerkezetének kialakítása #42

--- frontend/src/store/ErrorStore.js
+++ frontend/src/store/ErrorStore.js
@@ -3,7 +3,19 @@ import dispatcher from "../dispatcher/BookDispatcher";
 import {refresh} from '../dispatcher/ActionConstans';
 
 class ErrorStore extends EventEmitter{
+    _errorMsg = "";
 
+    emitChange(){
+        this.emit('Change');
+    }
+
+    addOnChangeListener(callback){
+        this.addListener('Change', callback);
+    }
+
+    removeOnChangeListener(callback){
+        this.removeListener('Change',callback);
+    }
 
 }
 const store = new ErrorStore();
