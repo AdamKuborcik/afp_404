500b2624277a396cd01d7784ce9192c165a0c58e
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-17 17:57:19 +0200

    Login Action alap szerkezetének megírása #43

---
+++ frontend/src/actions/Login.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import BookDispatcher from '../dispatcher/BookDispatcher';
+import * as actionConstants from '../dispatcher/ActionConstans'
+
+export const Login = () => {
+    axios.defaults.timeout = 1500;
+    axios.get('/login').then((resp) => {
+        BookDispatcher.dispatch({
+            action: actionConstants.refresh,
+            payload: resp.data
+        })
+    })
+}
