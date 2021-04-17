27c28483cfa9428b167bba53f263bcb3f59de5db
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-17 17:51:47 +0200

    dispatcher.register megírása #42

--- frontend/src/store/ErrorStore.js
+++ frontend/src/store/ErrorStore.js
@@ -1,6 +1,6 @@
 import {EventEmitter} from 'events';
 import dispatcher from "../dispatcher/BookDispatcher";
-import {refresh} from '../dispatcher/ActionConstans';
+import * as ActionConstans from "../dispatcher/ActionConstans";
 
 class ErrorStore extends EventEmitter{
     _errorMsg = "";
@@ -21,3 +21,17 @@ class ErrorStore extends EventEmitter{
 const store = new ErrorStore();
 export default store;
 
+dispatcher.register(({action,payload})=>{
+    console.log({action : action, payload : payload});
+    if(action !== ActionConstans.showError){
+        return;
+    }
+    store._errorMsg = payload;
+    store.emitChange();
+});
+
+dispatcher.register(({action})=>{
+    if(action !== ActionConstans.clearError) return;
+    store._errorMsg = "";
+    store.emitChange();
+});
