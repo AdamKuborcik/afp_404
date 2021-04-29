c051dcb3bc1dd57735f1607f848dcc2762d8783a
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-29 19:54:05 +0200

    Modify Book törzsének megírása #39

--- frontend/src/actions/ModifyBook.js
+++ frontend/src/actions/ModifyBook.js
@@ -2,6 +2,29 @@ import axios from 'axios';
 import BookDispatcher from '../dispatcher/BookDispatcher';
 import * as actionConstants from '../dispatcher/ActionConstans'
 
-export const ModifyBook = ({id}) => {
+export const ModifyBook = (value) => {
+    axios.defaults.timeout = 1500;
+    axios.put('/update',
+        {
+            id:value.modifyID,
+            data: {
+                author: value.author,
+                title: value.title,
+                genre: value.genre
+            }
+        })
+        .then((resp) => {
+            console.log("was a response")
 
+        })
+        .catch((error) => {
+                console.log(error);
+                BookDispatcher.dispatch(
+                    {
+                        action: actionConstants.showError,
+                        payload: null
+                    }
+                )
+            }
+        )
 }
