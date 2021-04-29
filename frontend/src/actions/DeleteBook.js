4273acf68f08c9ef426db2a566c5f311516205ed
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-29 19:46:04 +0200

    delete action törzsének megírása #38

--- frontend/src/actions/DeleteBook.js
+++ frontend/src/actions/DeleteBook.js
@@ -3,5 +3,15 @@ import BookDispatcher from '../dispatcher/BookDispatcher';
 import * as actionConstants from '../dispatcher/ActionConstans'
 
 export const deletBook = ({id}) =>{
+    axios.delete('/delete/'+id)
+        .then(() => {
+            BookDispatcher.dispatch({action : actionConstants.clearError});
+        })
+        .catch((err) => {
+            BookDispatcher.dispatch({
+                action : actionConstants.showError,
+                payload: `${err.response.status}-${err.response.statusText}: ${err.response.data.message}`
+            });
 
+        });
 }
