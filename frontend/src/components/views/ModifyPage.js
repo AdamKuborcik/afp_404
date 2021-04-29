7cadcc30552728f2748fb83ce573ada58a2e5b27
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-29 20:02:18 +0200

    import szekció megírása #50

---
+++ frontend/src/components/views/ModifyPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactModal from 'react-modal';
+import {Button} from "reactstrap";
+import * as Yup from "yup";
+import {Formik} from "formik";
+import {ModifyBook} from "../../actions/ModifyBook";
+
