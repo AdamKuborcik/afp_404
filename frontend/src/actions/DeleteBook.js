4567045aa0a75e0b82278eb617a37a7d500a8d87
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-29 19:43:27 +0200

    import sorok hozzáadása (dispatcher actionConstants és axios) #38

--- frontend/src/actions/DeleteBook.js
+++ frontend/src/actions/DeleteBook.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import BookDispatcher from '../dispatcher/BookDispatcher';
+import * as actionConstants from '../dispatcher/ActionConstans'
+
 export const deletBook = ({id}) =>{
 
 }
