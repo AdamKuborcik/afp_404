46b4b502c2fe1c3743b8f725d2fecd00ca1f3527
KomjatiEvelin <komjatievelin@gmail.com>
2021-05-05 10:52:57 +0200

    list all művelet megírása a backend Book.js-ben

--- backend/routes/Book.js
+++ backend/routes/Book.js
@@ -5,6 +5,17 @@ const url = "mongodb+srv://user1:4VUnYWQAUO8daRVB@cluster0.apeyl.mongodb.net/myF
 const mongodb = require('mongodb');
 
 //List All
+router.get('/listbooks', function(req, res, next) {
+    MongoClient.connect(url,function (err,db){
+        if(err) throw err;
+        let dbo=db.db("mydb");
+        dbo.collection("Book").find({}).toArray(function (err,result) {
+            if (err) throw err;
+            db.close();
+            res.send(result);
+        })
+    })
+});
 //Delete in id
 //Find one element
 router.get('/findOne',function (req,res) {
