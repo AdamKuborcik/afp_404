05d53e2335df04bddd7ed6723c8da3aa4a113e0e
KomjatiEvelin <komjatievelin@gmail.com>
2021-05-06 13:12:15 +0200

    AddBook view törzs megírása #46

--- frontend/src/components/views/AddBookPage.js
+++ frontend/src/components/views/AddBookPage.js
@@ -3,4 +3,86 @@ import {Formik} from "formik";
 import * as Yup from "yup";
 import {AddBook} from "../../actions/AddBook";
 
-export const AddBookPage  = () => ({});
+export const AddBookPage  = () => ( <Formik
+    initialValues={{author: "",title: "", genre:""}}
+    onSubmit={(values, {setSubmitting}) => {
+        setTimeout(() => {
+            console.log(values)
+            AddBook(values);
+            setSubmitting(false);
+        }, 1000);
+    }}
+
+    validationSchema={Yup.object().shape({
+        author: Yup.string()
+            .required(),
+        title: Yup.string()
+            .required(),
+        genre: Yup.string()
+            .required()
+    })}
+>
+    {props => {
+        const {
+            values,
+            touched,
+            errors,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit
+        } = props;
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="author">Author's name:</label>
+                    <input
+                        name="author"
+                        type="text"
+                        placeholder="Author Name"
+                        value={values.author}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={ errors.author && touched.author ? "form-control is-invalid" : "form-control"}
+                    />
+                    {errors.author && touched.author && (
+                        <div className={"invalid-feedback"}>{errors.author} </div>
+                    )}
+                </div>
+                <div className={"form-group"}>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        name="title"
+                        type="text"
+                        placeholder="Book Title"
+                        value={values.title}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={errors.title && touched.title ? "form-control is-invalid" : "form-control"}
+                    />
+                    {errors.title && touched.title && (
+                        <div className={"invalid-feedback"}>{errors.title}</div>
+                    )}
+                </div>
+                <div className={"form-group"}>
+                    <label htmlFor="genre">Genre:</label>
+                    <input
+                        name="genre"
+                        type="text"
+                        placeholder="Genre of Book"
+                        value={values.genre}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={errors.genre && touched.genre? "form-control is-invalid" : "form-control"}
+                    />
+                    {errors.genre && touched.genre && (
+                        <div className={"invalid-feedback"}>{errors.genre}</div>
+                    )}
+                </div>
+                <button type="submit" disabled={isSubmitting} className={"btn btn-primary"}>
+                    Add Book
+                </button>
+            </form>
+        );
+    }}
+</Formik>);
