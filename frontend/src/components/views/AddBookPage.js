fee2756a5ecfbcab619436982ef6b7a5f6cd53a0
KomjatiEvelin <komjatievelin@gmail.com>
2021-05-06 13:07:49 +0200

    AddBook view import és fejléc megírása #46

---
+++ frontend/src/components/views/AddBookPage.js
@@ -0,0 +1,6 @@
+import React , { Component } from 'react'
+import {Formik} from "formik";
+import * as Yup from "yup";
+import {AddBook} from "../../actions/AddBook";
+
+export const AddBookPage  = () => ({});
